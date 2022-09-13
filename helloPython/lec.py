0dbbb24658e61c131662d7616764729236d0d970
Miroslav540 <mir19972905@mail.ru>
2022-09-13 15:41:30 +0300

    make 1 task

---
+++ helloPython/lec.py
@@ -0,0 +1,25 @@
+#print("hello world")
+
+
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+# s = 'qwerty'
+
+# print(s)
+
+# list = ['1', '2', '3', 'hello']
+# print(list)
+
+# print('введи a')
+# a = int(input())
+# print('введи b')
+# b = int(input())
+# print(a, '+', b, '=', a+b)
+
+
+a = 2
+b = 8
+c = a + b
+print(c)
